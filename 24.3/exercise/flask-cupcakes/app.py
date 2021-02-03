722102693346cbc5fc76f78dff7079ef0a4a823b
Almir Campos <almircampos@gmailcom>
2021-02-03 03:20:04 -0800

    [U2403]Topics and [WIP]Exercise

---
+++ 24.3/exercise/flask-cupcakes/app.py
@@ -0,0 +1,135 @@
+"""Flask app for Cupcakes"""
+
+from flask import Flask, request, jsonify, render_template
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///unit_24'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = ""
+
+connect_db(app)
+
+# SEEDING
+
+db.drop_all()
+db.create_all()
+
+c1 = Cupcake(
+    flavor="cherry",
+    size="large",
+    rating=5,
+)
+
+c2 = Cupcake(
+    flavor="chocolate",
+    size="small",
+    rating=9,
+    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
+)
+
+c3 = Cupcake(
+    flavor="human flesh",
+    size="bite",
+    rating=5,
+    image="https://images.halloweencostumes.com/products/8191/1-1/womens-cupcake-costume.jpg"
+)
+
+db.session.add_all([c1, c2, c3])
+db.session.commit()
+
+
+# ROUTES
+
+@app.route("/")
+def root():
+    """Render homepage."""
+
+    return render_template("base.html")
+
+
+@app.route("/api/cupcakes")
+def list_cupcakes():
+    """Return all cupcakes in system.
+
+    Returns JSON like:
+        {cupcakes: [{id, flavor, rating, size, image}, ...]}
+    """
+
+    cupcakes = [cupcake.to_dict() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Add cupcake, and return data about new cupcake.
+
+    Returns JSON like:
+        {cupcake: [{id, flavor, rating, size, image}]}
+    """
+
+    data = request.json
+
+    cupcake = Cupcake(
+        flavor=data['flavor'],
+        rating=data['rating'],
+        size=data['size'],
+        image=data['image'] or None)
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    # POST requests should return HTTP status of 201 CREATED
+    return (jsonify(cupcake=cupcake.to_dict()), 201)
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def get_cupcake(cupcake_id):
+    """Return data on specific cupcake.
+
+    Returns JSON like:
+        {cupcake: [{id, flavor, rating, size, image}]}
+    """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    return jsonify(cupcake=cupcake.to_dict())
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["PATCH"])
+def update_cupcake(cupcake_id):
+    """Update cupcake from data in request. Return updated data.
+
+    Returns JSON like:
+        {cupcake: [{id, flavor, rating, size, image}]}
+    """
+
+    data = request.json
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    cupcake.flavor = data['flavor']
+    cupcake.rating = data['rating']
+    cupcake.size = data['size']
+    cupcake.image = data['image']
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    return jsonify(cupcake=cupcake.to_dict())
+
+
+@app.route("/api/cupcakes/<int:cupcake_id>", methods=["DELETE"])
+def remove_cupcake(cupcake_id):
+    """Delete cupcake and return confirmation message.
+
+    Returns JSON of {message: "Deleted"}
+    """
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+
+    db.session.delete(cupcake)
+    db.session.commit()
+
+    return jsonify(message="Deleted")
---
+++ 24.3/exercise/flask-cupcakes/static/cupcakes.js
@@ -0,0 +1,68 @@
+const BASE_URL = "http://localhost:5000/api";
+
+
+/** given data about a cupcake, generate html */
+
+function generateCupcakeHTML(cupcake) {
+  return `
+    <tr data-cupcake-id=${cupcake.id}>
+      <td class="align-middle text-center">
+        <img class="cupcake-img rounded-circle" src="${cupcake.image}" alt="(no image provided)">
+      </td>
+      <td class="align-middle">${cupcake.flavor}</td>
+      <td class="align-middle">${cupcake.size}</td>
+      <td class="align-middle">${cupcake.rating}</td>
+      <td class="align-middle"><img class="delete-button-icon" src="/static/tenor.gif"/></td>
+    </tr>
+  `;
+}
+
+
+/** put initial cupcakes on page. */
+
+async function showInitialCupcakes() {
+  const response = await axios.get(`${BASE_URL}/cupcakes`);
+
+  for (let cupcakeData of response.data.cupcakes) {
+    let newCupcake = $(generateCupcakeHTML(cupcakeData));
+    $("#cupcakes-list").append(newCupcake);
+  }
+}
+
+
+/** handle form for adding of new cupcakes */
+
+$("#new-cupcake-form").on("submit", async function (evt) {
+  evt.preventDefault();
+
+  let flavor = $("#form-flavor").val();
+  let rating = $("#form-rating").val();
+  let size = $("#form-size").val();
+  let image = $("#form-image").val();
+
+  const newCupcakeResponse = await axios.post(`${BASE_URL}/cupcakes`, {
+    flavor,
+    rating,
+    size,
+    image
+  });
+
+  let newCupcake = $(generateCupcakeHTML(newCupcakeResponse.data.cupcake));
+  $("#cupcakes-list").append(newCupcake);
+  $("#new-cupcake-form").trigger("reset");
+});
+
+
+/** handle clicking delete: delete cupcake */
+
+$("#cupcakes-list").on("click", ".delete-button", async function (evt) {
+  evt.preventDefault();
+  let $cupcake = $(evt.target).closest("div");
+  let cupcakeId = $cupcake.attr("data-cupcake-id");
+
+  await axios.delete(`${BASE_URL}/cupcakes/${cupcakeId}`);
+  $cupcake.remove();
+});
+
+
+$(showInitialCupcakes);
